513b11e5d5b7310a5f7d2bc4220a20ad3267c71b
mistfail <sinsheimer.alexandre@gmail.com>
2020-10-20 16:40:38 +0200

    init commit of the structure

---
+++ client/vue-application.js
@@ -0,0 +1,1 @@
+const Home = window.httpVueLoader('./components/Main.vue')
---
+++ server/routes/api.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const router = express.Router()
+const definitions = require('../data/definitions.js')
+
+module.exports = router
