8cfc9d78ff89451a833a21c9da897141b30cbc42
mistfail <sinsheimer.alexandre@gmail.com>
2020-11-03 15:59:52 +0100

    add signin with router and all also new page Connexion.vue

---
+++ client/components/Connexion.vue
@@ -0,0 +1,38 @@
+<template>
+  <form  id="user" @submit="signin">
+    <h2>Se connecter</h2>
+    <div>
+      <label for="email">
+        <input type="email" v-model="user.email" id="email" placeholder="email" required>
+      </label>
+    </div>
+    <div id="password">
+      <label for="mdp">
+        <input type="password" v-model="user.password" id="mdp" placeholder="mot de passe" required>
+      </label>
+    </div>
+    <button type="submit">Connexion</button>
+  </form>
+</template>
+
+<script>
+module.exports  = {
+  data() {
+    return {
+      user: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    signin () {
+      this.$emit('sign-in', this.user)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
