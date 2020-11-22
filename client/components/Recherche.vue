fa5628cf2e4c8b76c83fea13bf4fe8a185bf11d2
Benjamin ROECKER <benjamin.roecker@efrei.net>
2020-11-22 18:02:53 +0100

    Recherche.vue

--- client/components/Recherche.vue
+++ client/components/Recherche.vue
@@ -40,5 +40,39 @@ name: "Recherche",
 </script>
 
 <style scoped>
-
+article {
+  display: flex;
+  border: 2px solid black;
+  margin:60px 20% 30px 20%;
+  box-shadow:9px -9px black;
+  background-color: white;
+  border-radius: 5px;
+}
+.fun{
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+.upvote{
+  align-self: flex-end;
+  margin: 10px;
+  display: flex;
+  align-items: center;
+}
+.vote{
+  height: 20px;
+  margin: 5px;
+}
+.title{
+  border: 2px solid black;
+  border-radius: 10px;
+  width: 30%;
+  text-align: center;
+  margin-left: 10px;
+  padding: 5px;
+}
+.definition{
+  width: 80%;
+  margin-left: 10px;
+}
 </style>
